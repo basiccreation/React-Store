70a991871351e246bdfd4d94021017eae9834cc9
pia smith <basiccreation@yahoo.com>
2019-02-02 17:34:43 -0800

    added Cartlist to Cart 04:44:59

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,8 +3,8 @@ import Title from "./Title";
 import CartColumns from "./CartColumns"
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from "./context"
-
-
+import CartList from "./CartList";
+ 
 
 class Cart extends Component {
   render() {
@@ -18,6 +18,7 @@ class Cart extends Component {
               <React.Fragment>
                 <Title name="your" title="cart"/>
                 <CartColumns/> 
+                <CartList value = {value}/>
               </React.Fragment>               
                 );
         } else {
