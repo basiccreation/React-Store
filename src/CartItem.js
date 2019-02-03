e5cea3675d4915812cd459bdf189de8de8ce7b6a
pia smith <basiccreation@yahoo.com>
2019-02-02 18:12:01 -0800

    deleted div around incre/decre buttons

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,27 +12,26 @@ return(
         className="img-fluid"
         alt = "product"
         />
-        <div className="col-10 mx-auto col-lg-2">
+      <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">product :  </span>
         {title}
-        </div>
-        <div className="col-10 mx-auto col-lg-2">
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">price :  </span>
         {price} 
+      </div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+            <span className="btn btn-black mx-1" onClick={()=>decrement(id)}>-</span>
+            <span className="btn btn-black mx-1">{count}</span>
+            <span className="btn btn-black mx-1" onClick={()=>increment(id)}>+</span>
         </div>
-        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
-          <div className="d-flex justify-content-center">
-            <div>
-              <span className="btn btn-black mx-1" onClick={()=>decrement(id)}>-</span>
-            
-            </div>
-          </div>
-        </div>
+      </div>
 
 
-        <div className="col-10 mx-auto col-lg-2">
-          Remove button
-        </div>
+      <div className="col-10 mx-auto col-lg-2">
+        Remove button
+      </div>
         <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">Item total :  </span>
         {total}
