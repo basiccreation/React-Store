94aee80957a7a4212bbc3c467f41b39b4b1df5d5
pia smith <basiccreation@yahoo.com>
2019-02-03 17:34:26 -0800

    added increment functionality

--- src/context.js
+++ src/context.js
@@ -74,7 +74,16 @@ getItem = (id) => {
 
 
 increment = (id) => {
-  console.log("this is increment method");  
+  let tempCart = [...this.state.cart];
+  const selectedProduct = tempCart.find(item => item.id === id);
+  const index = tempCart. indexOf(selectedProduct);
+  const product = tempCart[index];  
+
+  product.count = product.count + 1;
+  product.total = product.count * product.price;
+  this.setState(() => {return {cart:[...tempCart]}},()=> {
+    this.addTotals()
+  })
 }
 
 decrement = (id) => {
