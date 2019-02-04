4815f1dabf5284b4d545d768318b4405d3419f9f
pia smith <basiccreation@yahoo.com>
2019-02-03 19:10:14 -0800

    beautified  404 page 05:46:17

--- src/Default.jsx
+++ src/Default.jsx
@@ -7,8 +7,7 @@ return (
         <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
             <h3>page not found</h3>
             <h3>-</h3>
-            <h1 className="display-3">404 
-        <span className = "text-danger"> error</span></h1>
+            <h1 className="display-3">404 <span className = "text-danger"> error</span></h1>
             <h3>-</h3>
             <h4>the requested page, <br/>localhost:3000<span className = "text-danger">{this.props.location.pathname}</span> not found</h4>
         </div>
